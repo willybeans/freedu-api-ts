2adfb5fd42fc6bf7aa65cd0df703d82a7cce470c
Will Wedmedyk <willwedmedyk@gmail.com>
2023-06-26 16:34:56 -0400

    ww/clean-test-file

---
+++ src/server.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import { app } from '../src/app';
+
+// afterAll(done => {
+// //    app.close();
+//     done();
+// });
+
+describe('Example Test', () => {
+  it('should return "healthcheck"', async () => {
+    const response = await request(app).get('/healthcheck');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('healthcheck');
+  });
+});
