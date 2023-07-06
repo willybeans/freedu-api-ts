1a20170804825f6f2e1d155b13e43b87c1a5d3a3
Will Wedmedyk <willwedmedyk@gmail.com>
2023-07-06 12:28:57 -0400

    ww/initial websocket

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
 import express, { type Express } from 'express';
 import routes from './routes/index';
+import expressWs from 'express-ws';
 // import session from 'express-session';
 // import enforceSSL from 'express-sslify';
-// import dotenv from 'dotenv';
 
 import morganConfig from './config/morganConfig';
 
 export const app: Express = express();
-
-// app.use('/', routes);
-// import routes from './routes/index';
-
-// dotenv.config();
-
-// const app: Express = express();
-// const port = process.env.PORT
+const ews = expressWs(app);
 
 app.use(morganConfig);
 // Redirect HTTP to HTTPS (if you have a valid SSL certificate)
@@ -32,5 +25,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
+
+ews.app.ws('/games/:gameId', function (ws, req) {
+  const gameId = req.params.gameId;
+  ws.on('message', function (msg) {
+    ws.send(JSON.stringify(msg) + ' ' + gameId);
+  });
+});
 
 // Other middleware and routes
