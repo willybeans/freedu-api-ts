37de146f22e8ff51620a00e7253ed8eee64d36bf
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-16 19:32:10 -0400

    ww/types for game

---
+++ src/types/game.ts
@@ -0,0 +1,54 @@
+import { type GamePlayer, type Players, type TableCard } from './players';
+
+export interface TurnIndicatorProps {
+  currentPlayer: string;
+  players: string[];
+}
+
+export interface GameBoardProps {
+  players: string[];
+}
+
+export interface PlayerScore {
+  id: string;
+  score: number;
+}
+
+export interface ScoreboardProps {
+  scores: PlayerScore[];
+}
+
+export interface GameState {
+  inPlay: boolean;
+  shuffledDeck: string[];
+  currentCardsOnTable: TableCard[];
+  currentPlayer: number;
+  picker: string;
+  secretTeam: string[];
+  otherTeam: string[];
+  blindCards: string[];
+  setScoreMode: 'leaster' | 'doubler' | 'picker';
+}
+
+export interface Game {
+  players: GamePlayer[];
+  shuffledDeck: string[];
+  currentCardsOnTable: TableCard[];
+  currentPlayer: number;
+  picker: string;
+  secretTeam: string[];
+  otherTeam: string[];
+  blindCards: string[];
+  setScoreMode: 'leaster' | 'doubler' | 'picker'; // is this right? picker? im not sure
+  newDeck: () => void;
+  moveToNext: () => void;
+  setPicker: (playerId: string, players: Players) => void;
+  setSecretAndOtherTeam: (namedCard: string) => void;
+  dealCards: () => void;
+  tableReceiveCard: () => void;
+  calculateHandWinner: () => void;
+  calculateScore: () => void;
+  resetPlayersForNewTurn: () => void;
+  resetGameForNewTurn: () => void;
+  resetAll: () => void;
+}
